8ec209fd3eb1f41472875a2fe3463915b6dd770e
mohamed larbi benchouala <mohamedlarbi.benchouala@gmail.com>
2022-08-19 11:09:23 +0200

    watchlist

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+List.destroy_all
+Bookmark.destroy_all
+Movie.destroy_all
+require 'json'
+require 'open-uri'
+url = "https://tmdb.lewagon.com/movie/top_rated"
+result = JSON.parse(URI.open(url).read)
+result["results"].each do |result_1|
+  Movie.create!(title: result_1["original_title"], overview: result_1["overview"], poster_url: "https://image.tmdb.org/t/p/original/#{result_1["poster_path"]}.jpg", rating:result_1["vote_average"])
+end
